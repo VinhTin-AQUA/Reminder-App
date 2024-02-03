e49743df4a60b2a4da078383f3612e53f311f89c
VinhTin-AQUA <tinhovinh@gmail.com>
2024-02-03 23:28:02 +0700

    add app built

---
+++ ReminderApp/Stores/Urls.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApp.Utils
+{
+    public static class Urls
+    {
+        private static string dataUrl = @"./Data";
+        private static string remiderUrl = "/reminderInfo.json";
+        private static string idUrl = "/identity.txt";
+
+        private static string resourceUrl = @"./Resources";
+        private static string imageUrl = @"/images";
+        private static string reminderBackground = @"/reminder_background.png";
+        private static string reminderIcon = @"/reminder_icon.ico";
+
+        // data
+        public static string DataUrl
+        {
+            get { return dataUrl; }
+        }
+
+        public static string RemiderUrl
+        {
+            get 
+            { 
+                return dataUrl + remiderUrl; 
+            }
+        }
+
+        public static string IdUrl
+        {
+            get
+            {
+                return dataUrl + idUrl;
+            }
+        }
+
+        // resource
+        public static string ResourceUrl
+        {
+            get { return resourceUrl; }
+        }
+
+        public static string ImageUrl
+        {
+            get { return resourceUrl + imageUrl; }
+        }
+
+        public static string ReminderBackground
+        {
+            get { return resourceUrl + imageUrl + reminderBackground; }
+        }
+
+        public static string ReminderIcon
+        {
+            get { return resourceUrl + imageUrl + reminderIcon; }
+        }
+    }
+}
